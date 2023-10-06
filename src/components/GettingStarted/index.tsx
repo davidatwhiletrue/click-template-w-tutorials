e8fdaef0bcdb3b9ac175a4ff9d8d0d5c75c752a0
David Hernando <david.hernando@make.services>
2023-10-06 15:33:01 +0200

    wip

--- src/components/GettingStarted/index.tsx
+++ src/components/GettingStarted/index.tsx
@@ -4,6 +4,7 @@ import { AccountType } from '@make-software/csprclick-types';
 import Account from './components/Account';
 import BuyMeACoffee from './components/BuyMeACoffee';
 import CustomMenuItems from "./components/CustomMenuItems";
+import {SectionContainer} from "./components";
 
 const AccountInfoContainer = styled.div(({ theme }) =>
 	theme.withMedia({
@@ -21,13 +22,25 @@ export type GettingStartedProps = {
 export const GettingStarted = ({ account }: GettingStartedProps) => {
 	return (
 		<AccountInfoContainer>
-			<h2>🎉 Your user has signed in! What next?</h2>
+			<h2>🎉 Awesome! Your user has signed in! What next?</h2>
+			<SectionContainer background={'unset'}>
+				<span>
+					You&apos;re almost ready to start working on your app. But first, let&apos;s see a couple of features in the SDK
+					you&apos;ll use often.
+				</span>
+			</SectionContainer>
 			<h3>🆔 Get your user´s account and update your app</h3>
 			<Account />
 			<h3>🔧 Customize the account menu</h3>
 			<CustomMenuItems />
 			<h3>☕ Buy Alice a coffee on testnet</h3>
 			<BuyMeACoffee />
+			<h3>🔥 Happy hacking!</h3>
+			<SectionContainer background={'unset'}>
+				<span>Finally! Time to focus on your new project! And, remember, you may find guides and examples in <a href={'https://docs.cspr.click'}
+					target={'_blank'} rel="noreferrer">our documentation</a>. Or you can reach to us in our telegram.
+				</span>
+			</SectionContainer>
 		</AccountInfoContainer>
 	);
 };
