2472ba7b29b7bd1747a7b48e4d0ab15598a95a76
David Hernando <david.hernando@make.services>
2023-11-08 09:41:37 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import App from './App';
 const clickOptions: CsprClickInitOptions = {
 	appName: 'CSPR.app',
 	contentMode: CONTENT_MODE.IFRAME,
-	providers: ['casper-wallet', 'ledger', 'torus-wallet', 'casperdash', 'metamask-snap', 'casper-signer'],
+	providers: ['casper-wallet', 'ledger', 'casperdash', 'metamask-snap', 'torus-wallet', 'casper-signer'],
 	appId: 'csprclick-template',
 	csprclickHost: 'https://casper-click-static.dev.make.services',
 };
